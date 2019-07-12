5b372483225f69045f9974923c484d6b9824649a
Joon Ho Cho <joonho1101@gmail.com>
2019-07-12 10:10:19 -0700

    up deps

--- src/http.ts
+++ src/http.ts
@@ -1,6 +1,31 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { ParsedUrlQuery } from 'querystring';
+import { parse } from 'url';
 import { HttpError } from './httpError';
-export * from 'notil/dist/node/http';
+
+export const getRequestQuery = (req: IncomingMessage): ParsedUrlQuery =>
+  parse(req.url || '', true).query;
+
+export const getRequestBody = (req: IncomingMessage): Promise<Buffer> =>
+  new Promise((resolve, reject): void => {
+    const buffers: Buffer[] = [];
+    req.on('data', (chunk) => {
+      buffers.push(chunk);
+    });
+    req.on('end', () => resolve(Buffer.concat(buffers)));
+    req.on('error', reject);
+  });
+
+export const getRequestBodyJSON = <T>(
+  req: IncomingMessage
+): Promise<T | undefined> =>
+  getRequestBody(req).then((data) => {
+    try {
+      return JSON.parse(data.toString('utf8'));
+    } catch (e) {
+      return undefined;
+    }
+  });
 
 export const sendHttpErrorResponse = (
   res: ServerResponse,
